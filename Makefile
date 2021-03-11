9f5bf05c543409eba047a889261e064816f84d2b
Otto Laitila <otto@laiti.la>
2021-03-11 12:56:07 +0200

    Add thymeleaf for templating, generate sample document

--- Makefile
+++ Makefile
@@ -1,11 +1,13 @@
 IMAGE = crisp-pdfmaker
-RUN = docker run --volume `pwd`:/app --rm -ti $(IMAGE)
+RUN = docker run --volume `pwd`:/app --volume `pwd`/pdf-output:/pdf-output --rm -ti $(IMAGE)
 
 build:
 	docker build -t $(IMAGE) .
 sh:
 	$(RUN) bash
 run:
-	$(RUN)
+	$(RUN) gradle run
+clean:
+	$(RUN) gradle clean
 
-.PHONY: build sh run
+.PHONY: build sh run clean
--- app/src/main/kotlin/crisp/pdfmaker/App.kt
+++ app/src/main/kotlin/crisp/pdfmaker/App.kt
@@ -3,22 +3,39 @@ package crisp.pdfmaker
 import com.openhtmltopdf.pdfboxout.PdfRendererBuilder
 import java.nio.file.Files
 import java.nio.file.Paths
+import org.thymeleaf.TemplateEngine
+import org.thymeleaf.context.Context
+import org.thymeleaf.templatemode.TemplateMode
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver
+import org.thymeleaf.templateresolver.ITemplateResolver
 
 fun main() {
-    val builder = PdfRendererBuilder()
-    builder.useFastMode()
-    builder.withUri(template("test"))
+    val engine = TemplateEngine()
+    engine.addTemplateResolver(templateResolver())
 
-    Files.newOutputStream(Paths.get("/tmp/file.pdf")).use { out ->
+    val context = Context()
+    context.setVariable("name", "otto")
+
+    val htmlContent = engine.process("test", context)
+
+    val builder = PdfRendererBuilder().apply {
+        useFastMode()
+        withHtmlContent(htmlContent, null)
+    }
+
+    Files.newOutputStream(Paths.get("/pdf-output/file.pdf")).use { out ->
         builder.toStream(out)
         builder.run()
     }
 }
 
-private fun template(name: String): String {
-    val uri = object {}.javaClass
-        .classLoader
-        .getResource("templates/${name}.htm")
-
-    return uri.toString()
+private fun templateResolver(): ITemplateResolver {
+    return ClassLoaderTemplateResolver().apply {
+        prefix = "templates/"
+        suffix = ".htm"
+        templateMode = TemplateMode.HTML
+        characterEncoding = "UTF-8"
+        order = 1
+        checkExistence = true
+    }
 }

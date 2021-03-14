9701e4706ba1b6ffa2bc3c08abb4a19a3f983805
Otto Laitila <otto@laiti.la>
2021-03-14 15:48:24 +0200

    Add upload to S3

--- app/src/main/kotlin/crisp/pdfmaker/Handler.kt
+++ app/src/main/kotlin/crisp/pdfmaker/Handler.kt
@@ -3,34 +3,53 @@ package crisp.pdfmaker
 import com.amazonaws.services.lambda.runtime.Context
 import com.amazonaws.services.lambda.runtime.RequestHandler
 import java.io.ByteArrayOutputStream
-import java.util.*
+import software.amazon.awssdk.core.sync.RequestBody
+import software.amazon.awssdk.regions.Region
+import software.amazon.awssdk.services.s3.S3Client
+import software.amazon.awssdk.services.s3.model.PutObjectRequest
 
 class Handler : RequestHandler<Map<String, Any>, Map<String, Any>> {
     override fun handleRequest(event: Map<String, Any>, context: Context): Map<String, Any> {
-        context.logger.log("Got $event")
+        val logger = context.logger
+        logger.log("Got $event")
 
-        val template = event["template"] as String
-        val data = event["data"] as Map<String, String>
+        val s3BucketName = System.getenv("BUCKET_NAME")
+
+        val body = event["body"] as Map<*, *>
+
+        val requestId = body["request-id"] as String
+        val filename = body["filename"] as String
+        val template = body["template"] as String
+        val data = body["data"] as Map<String, String>
 
         val pdfMaker = PdfMaker()
-        val stream = ByteArrayOutputStream()
+        val outputStream = ByteArrayOutputStream()
 
         pdfMaker.makePdf(
             template,
             data,
-            stream
+            outputStream
         )
 
-        val encoded = Base64.getEncoder().encode(stream.toByteArray())
-        val base64EncodedPdfContent = String(encoded)
+        val s3 = S3Client.builder()
+            .region(Region.EU_CENTRAL_1)
+            .build()
+
+        val putObjectRequest = PutObjectRequest.builder()
+            .bucket(s3BucketName)
+            .key(filename)
+            .build()
+
+        s3.putObject(
+            putObjectRequest,
+            RequestBody.fromBytes(outputStream.toByteArray())
+        )
 
         return mapOf(
-            "statusCode" to 200,
-            "headers" to listOf(
-                mapOf("Content-type" to "application/pdf")
-            ),
-            "body" to base64EncodedPdfContent,
-            "isBase64Encoded" to true
+            "filename" to filename,
+            "bucket" to s3BucketName,
+            "key" to filename,
+            "requestId" to requestId
         )
     }
 }
